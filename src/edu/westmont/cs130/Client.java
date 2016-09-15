c9c912baed39179b59d2287444bba6747dd37b49
James Solum <jsolum@westmont.edu>
2016-09-15 15:05:57 -0700

    Just added another println as a test

--- src/edu/westmont/cs130/Client.java
+++ src/edu/westmont/cs130/Client.java
@@ -41,6 +41,7 @@ public class Client {
 	
 	public static void main(String[] args){
 		System.out.println(“Hi this is James Solum”);
+		System.out.println("This is a test friends");
 		new Client("192.168.1.97", 8888);
 	}
 }
